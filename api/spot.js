d1ab7c2c7e72c7c298e6b472a2517d302d486e91
kyle jeske <kylejeske@gmail.com>
2013-05-08 17:23:29 -0400

    Update for Spot API failing on checkin failure, added proper error handling on uncaught exceptions, updated frontend to handle errors better too

--- api/spot.js
+++ api/spot.js
@@ -171,8 +171,8 @@
 		console.log("error occured: " + JSON.stringify(r) + "," + JSON.stringify(v));
 	});
 
-	server.on('uncaughtException', function(err) {
-		console.error("Uncaught error occured: " + JSON.stringify(err));
+	server.on('uncaughtException', function(req, res, location, err) {
+		console.log("Uncaught error exception: " + err);
 	});
 
 	server.listen(restPort, function() {
@@ -817,52 +817,50 @@
 					res.send('Error validating spot schema:' + JSON.stringify(valid));
 					return;
 				} else {
-					try {
-						var query = {
-							where: {
-								spotId: parseInt(id)
-							}
-						};
-						Datastore.records.object("Spot", query, function(err, response, body){
-							if (body.length == 0) {
-								res.send("Umm your spot id wasnt found. weird.");
-								res.end(400);
-							} else {
-								var objectId = body[0].objectId;
-								json.spotPointer = {"__type":"Pointer","className":"Spot","objectId": objectId};
-								json.spotId = id;
+					var query = {
+						where: {
+							spotId: parseInt(id)
+						}
+					};
+					console.log(query);
+					Datastore.records.object("Spot", query, function(err, response, body){
+						if (body.length == 0) {
+							res.end("Umm your spot id wasnt found. weird.");
+							return;
+						} else {
+							var objectId = body[0].objectId;
+							json.spotPointer = {"__type":"Pointer","className":"Spot","objectId": objectId};
+							json.spotId = id;
 
-								var query = {
-									where: {
-										session_id: json.userId
-									}
-								};
+							var query = {
+								where: {
+									session_id: json.userId
+								}
+							};
 
-								Datastore.records.object("Profiles", query, function(err, response, body){
-									if (body.length == 0) {
-										res.send("umm couldnt find your profile .. weird.");
-										res.send(400);
-									} else {
+							Datastore.records.object("Profiles", query, function(err, response, body){
+								if (body.length == 0) {
+									res.statusCode = 400;
+									res.end("umm couldnt find your profile .. weird.");
+//									res.send(400);
+									return false;
+								} else {
 
-										var objectId = body[0].objectId;
-										json.profilePointer = {"__type":"Pointer","className":"Profiles","objectId": objectId};
-									
-										Datastore.records.createobject("Checkin", json, function(err, response){
-											// @todo, check check it complete, check for others in the same area
-											res.send(200, 'You\'ve been checked into this Spot.');
-										});
-									}
-								});
-							}
-						});
-					} catch (e) {
-						console.log("An unexpected error occured in the SpotAPI: " + JSON.stringify(e));
-					}
+									var objectId = body[0].objectId;
+									json.profilePointer = {"__type":"Pointer","className":"Profiles","objectId": objectId};
+								
+									Datastore.records.createobject("Checkin", json, function(err, response){
+										// @todo, check check it complete, check for others in the same area
+										res.send(200, 'You\'ve been checked into this Spot.');
+									});
+								}
+							});
+						}
+					});
 				}
 			} catch (e) {
 				console.log("Unexpected error occured: " + JSON.stringify(e));
 				res.statusCode = 400;
-				res.send(e);
 			}
 		});
 	});
--- public/js/lib/uploader.xss.js
+++ public/js/lib/uploader.xss.js
@@ -1,4 +1,7 @@
 (function() {
+	if (!document.body) {
+		return;
+	}
     var match = /(\{.+\}).+/.exec(document.body.innerHTML);
     if (match) {
         parent.postMessage(match[1], '*');
