e2f6286deddf8ce866640ba0dcd6446ea10a968a
kyle jeske <me@kylejeske.com>
2013-06-20 08:30:17 -0400

    added example/server bit

---
+++ frontend/poet/examples/_newposts/contact-us.md
@@ -0,0 +1,19 @@
+{{{
+    "title" : "Contact Us",
+    "tags"  : [],
+    "category" : "Who we are",
+    "date" : "01-02-2013",
+    "hide_main" : true,
+    "hide_date" : true
+}}}
+
+### Want to get in touch with us?
+
+
+You can reach us at via email at 
+#### [support@sparky.io](mailto:support@sparky.io)
+
+or give us a call at 
+#### 1 (855) 620-7730
+
+<a id='__imgButtonClickToCall' href='#'  onclick="window.open('http://myaccount.onebox.com/SECURE/C2CWidget/ClickToConnectSession.aspx?key=875ab190-b32b-49d3-a8eb-fe657aa93865&evoice=1','','width=570,height=280, left=40, top=100, resizable=no,scroll=yes,status=no,titlebar=no,toolbar=no,addressbar=no,copyhistory=no,navigationtoolbar=no')"  style='cursor:pointer;border:none'><img  src='http://myaccount.onebox.com/SECURE/Images/ClickToCallWidget/SystemImages3_h.jpg' alt='Click to open call me page' id='__imgClickToCall' style='border:0px; border-radius: 10px;' /></a>
---
+++ frontend/poet/examples/configuredSetup.js
@@ -0,0 +1,40 @@
+var
+  express  = require( 'express' ),
+  moment   = require( 'moment' ),
+  app      = express(),
+  poet     = require( '../lib/poet' )( app );
+
+poet.set({
+  postsPerPage : 3,
+  posts        : './_newposts',
+  metaFormat   : 'json'
+}).createPostRoute( '/:post.html', 'post' )
+  .createSitemapRoute('/sitemap.xml')
+  .createPageRoute( '/pagination/:page', 'page' )
+  .createTagRoute( '/mytags/:tag', 'tag' )
+  .createCategoryRoute( '/mycategories/:category', 'category' )
+  .init();
+
+app.set( 'view engine', 'jade' );
+app.set( 'views', __dirname + '/views' );
+app.use( express.static( require('path').resolve(__dirname) + '/public' ));
+app.use( app.router );
+
+app.get( '/services.html', function(req, res) {
+    res.render('services');
+});
+
+app.get( '/blog.html', function(req, res) {
+    res.render('blog');
+});
+
+app.get( '/', function ( req, res ) { 
+    res.render( 'index' ) 
+});
+
+app.use(function(req, res, next){
+    res.status(404);
+    res.redirect("/");    
+});
+
+app.listen( 3000 );
