ffffc13185339d1e293ad9ca3bd1e42f11ad6324
kyle jeske <kylejeske@gmail.com>
2013-07-17 16:36:56 -0400

    Updates to graph generator - added in new sync-exec system

--- crons/graph_images.js
+++ crons/graph_images.js
@@ -1,6 +1,7 @@
 var   cronJob = require('cron').CronJob
     , exec = require('child_process').exec
-    , syncExec = require('exec-sync')
+    , syncExecold = require('exec-sync')
+    , syncExec = require("exec-plan").ExecPlan
     , Datastore = require('../services/DataStore.js')
     , nconf = require('nconf')
     , sleep = require('sleep')
@@ -10,33 +11,55 @@ var   cronJob = require('cron').CronJob
 // CronTime: 00 * * * * * => once per minute
 // CronTime: 00 00 * * * * => once per hour
 
-var job = new cronJob({
-  cronTime: '00 * * * * *',
-  onTick: function() {
-    var queryParams = {};
 
-	// Use DataStore Instead
-	Datastore.records.object("Spot", queryParams, function(err, response, body, success) {
-        if (body.length > 0) {
-            var counter = 0;
-            var spot_id;
-            for (item in body) {
-                if (counter == 10) {
-                    counter = 0;
-                    console.log('waiting for next batch...');
-                    sleep.sleep(1);
+try {
+    var job = new cronJob({
+      cronTime: '00 00 * * * *',
+      onTick: function() {
+        var queryParams = {};
+
+        var running = 0;
+        syncPlan = new syncExec();
+        
+        syncPlan.on("execerror", function(err, strerr) {
+            console.log(err);
+            console.log(strerr);
+        });
+        
+        syncPlan.on("complete", function(stdout) {
+            var running = 0;
+            console.log(stdout);
+            console.log("set complete");
+        });
+        
+        syncPlan.on("finished", function() {
+            var running = 0;
+            console.log("set finished");
+        });
+
+    	// Use DataStore Instead
+    	Datastore.records.object("Spot", queryParams, function(err, response, body, success) {
+            if (body.length > 0) {
+                var counter = 0;
+                var spot_id;
+                for (item in body) {
+                    var query, picture_path;
+                    spot_id = body[item].spotId;
+                    // @NOTE - This path must be relative to the FIRST LEVEL, /var/http/www.kitecaster/ <- first would be the first level
+                    picture_path = require('path').resolve('./public/media/raw/');
+                    script_path = require('path').resolve(__dirname + '/scripts/graph_generate.js');
+                    query = "phantomjs " + script_path + " http://local.kitecaster.com/main/r/system-generated/spot-" + spot_id + ".html " + picture_path + "/spot-" + spot_id + "-auto.png";
+                    syncPlan.add(query);
                 }
-                spot_id = body[item].spotId;
-                syncExec("phantomjs scripts/graph_generate.js http://local.kitecaster.com/main/r/system-generated/spot-" + spot_id + ".html ../public/media/raw/spot-" + spot_id + "-auto.png", function(){
-                    console.log(arguments);
-                })
-                counter += 1;
+                syncPlan.execute();
             }
-        }
-
-	});
-
-  },
-  start: true
-});
-job.start();
+    
+    	});
+    
+      },
+      start: true
+    });
+    job.start();
+} catch (e) {
+    console.log(e);
+}
